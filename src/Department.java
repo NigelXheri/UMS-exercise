adfae257b53c811671789745776cdd8d299bc219
NigelXheri <nigelxheri@gmail.com>
2025-11-05 20:51:27 +0100

    added class Department

---
+++ src/Department.java
@@ -0,0 +1,82 @@
+public class Department {
+
+    private String departmentName;
+    private String location;
+    private int numberOfFaculty;
+    private String departmentHead;
+
+
+    public Department() {
+        this.departmentName = "";
+        this.location = "";
+        this.numberOfFaculty = 0;
+        this.departmentHead = "";
+    }
+
+    public Department(String departmentName, String location, int numberOfFaculty, String departmentHead) {
+        this.departmentName = departmentName;
+        this.location = location;
+        this.numberOfFaculty = numberOfFaculty;
+        this.departmentHead = departmentHead;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getNumberOfFaculty() {
+        return numberOfFaculty;
+    }
+
+    public String getDepartmentHead() {
+        return departmentHead;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setNumberOfFaculty(int numberOfFaculty) {
+        this.numberOfFaculty = numberOfFaculty;
+    }
+
+    public void setDepartmentHead(String departmentHead) {
+        this.departmentHead = departmentHead;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "departmentName='" + departmentName + '\'' +
+                ", location='" + location + '\'' +
+                ", numberOfFaculty=" + numberOfFaculty +
+                ", departmentHead='" + departmentHead + '\'' +
+                '}';
+    }
+
+    public void addFaculty(){
+        numberOfFaculty++;
+    }
+
+    public boolean removeFaculty(){
+        if (numberOfFaculty <= 0){
+            System.out.println("Cannot remove faculty because there isn't any");
+            return false;
+        }
+        numberOfFaculty--;
+        return true;
+    }
+
+    public boolean isLargeDepartment(){
+        return numberOfFaculty > 20;
+    }
+
+}
